fe17d48c47297e8ba06a48442116a9d1c74f2d94
James Lyne <jim+github@not-null.co.uk>
2021-07-07 22:03:39 +0100

    Migrate to gradle, java 16

--- src/main/java/net/teamonster/tealimit/Main.java
+++ src/main/java/net/teamonster/tealimit/Main.java
@@ -9,78 +9,58 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
-public class Main extends JavaPlugin implements Listener
-{
-    private int breedLimit;
-    private int naturalLimit;
-    private int range;
-    private int spawnEggLimit;
-    private int spawnerLimit;
+public class Main extends JavaPlugin implements Listener {
+	private int breedLimit;
+	private int naturalLimit;
+	private int range;
+	private int spawnEggLimit;
+	private int spawnerLimit;
 
-    @Override
-    public void onEnable()
-    {
-        getServer().getPluginManager().registerEvents(this, this);
-        getConfig().options().copyDefaults(true);
-        saveConfig();
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 
-        this.breedLimit = getConfig().getInt("breed-limit");
-        this.naturalLimit = getConfig().getInt("natural-limit");
-        this.range = getConfig().getInt("range");
-        this.spawnEggLimit = getConfig().getInt("spawnegg-limit");
-        this.spawnerLimit = getConfig().getInt("spawner-limit");
-    }
+		this.breedLimit = getConfig().getInt("breed-limit");
+		this.naturalLimit = getConfig().getInt("natural-limit");
+		this.range = getConfig().getInt("range");
+		this.spawnEggLimit = getConfig().getInt("spawnegg-limit");
+		this.spawnerLimit = getConfig().getInt("spawner-limit");
+	}
 
-    @EventHandler
-    public void onCreatureSpawn(CreatureSpawnEvent event)
-    {
-        switch(event.getSpawnReason())
-        {
-            case BREEDING:
-            case EGG:
-            case DISPENSE_EGG:
-            {
-                if(entityLimit(event.getEntity(), this.breedLimit))
-                    event.setCancelled(true);
-                break;
-            }
+	@EventHandler
+	public void onCreatureSpawn(CreatureSpawnEvent event) {
+		switch (event.getSpawnReason()) {
+			case BREEDING, EGG, DISPENSE_EGG -> {
+				if (entityLimit(event.getEntity(), this.breedLimit))
+					event.setCancelled(true);
+			}
+			case NATURAL, NETHER_PORTAL -> {
+				if (entityLimit(event.getEntity(), this.naturalLimit))
+					event.setCancelled(true);
+			}
+			case SPAWNER -> {
+				if (entityLimit(event.getEntity(), this.spawnerLimit))
+					event.setCancelled(true);
+			}
+			case SPAWNER_EGG -> {
+				if (entityLimit(event.getEntity(), this.spawnEggLimit))
+					event.setCancelled(true);
+			}
+		}
+	}
 
-            case NATURAL:
-            case NETHER_PORTAL:
-            {
-                if(entityLimit(event.getEntity(), this.naturalLimit))
-                    event.setCancelled(true);
-                break;
-            }
+	private boolean entityLimit(Entity entity, int limit) {
+		List<Entity> entityList = entity.getNearbyEntities(this.range, 255.0d, this.range);
+		EntityType entityType = entity.getType();
+		int count = 0;
 
-            case SPAWNER:
-            {
-                if(entityLimit(event.getEntity(), this.spawnerLimit))
-                    event.setCancelled(true);
-                break;
-            }
+		for (Entity value : entityList) {
+			if (value.getType() == entityType)
+				count++;
+		}
 
-            case SPAWNER_EGG:
-            {
-                if(entityLimit(event.getEntity(), this.spawnEggLimit))
-                    event.setCancelled(true);
-                break;
-            }
-        }
-    }
-
-    private boolean entityLimit(Entity entity, int limit)
-    {
-        List<Entity> entityList = entity.getNearbyEntities(this.range, 255.0d, this.range);
-        EntityType entityType = entity.getType();
-        int count = 0;
-
-        for(Entity value : entityList)
-        {
-            if(value.getType() == entityType)
-                count++;
-        }
-
-        return count > limit;
-    }
+		return count > limit;
+	}
 }
